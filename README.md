106605dc6aedeac089a90a3802cbf8a0aead1f3f
vaibhavpatidar522 <vaibhavpatidar522@gmail.com>
2023-04-11 11:41:40 +0530

    first commit

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
-[![HitCount](http://hits.dwyl.io/rbhatia46/React-Portfolio.svg)](http://hits.dwyl.io/rbhatia46/React-Portfolio)
-![Star this repository](https://img.shields.io/github/stars/rbhatia46/React-Portfolio?style=social)
 
+## Portfolio using React js and styled-components 2023
 
-A simple SPA Portfolio template for developer/designers built with React. Use it to showcase your work, testimonials and other information to clients.
+<img src="https://res.cloudinary.com/ghazni/image/upload/v1678467366/portfolio_6_tg19nz.png" alt="banner"/>
 
-## Preview
-![Preview](https://image.ibb.co/e5uBf0/Capture.png)
+## Demo
+[Demo](https://uxui-profile.netlify.app/)
 
-## How to use
-1. Clone/Download the repo.
-2. Run  ``` npm install ```.
-3. Change the values in ```src/resumeData.js``` to suit your use-case.
-4. Run ```npm start``` to spin the up the local dev server port 3000.(http://localhost:3000).
-5. Make required changes in ```src/resumeData.js``` to suit your needs.
+[Tutorial Video Link](https://youtu.be/xbyoqfwCFE8)
 
+## Features
+
+- Animated navbar
+- Slider
+- Animation on scroll
+- Responsive
+
+## Description
+
+This portfolio is build using React JS, Styled-Components and react awesome reveal library. People can see the owners services, projects, clients ideas and also has a contact form for people to get in touch with. This project has a youtube video tutorial that teaches the React newbies about React Hook, React-Slick and responsive design.
+
+Developed By Milad Amiri 2023.
+Watch more videos from me [Milad Tech](https://www.youtube.com/@miladtech2844)
+
+## Technologies 
+
+- [React JS](https://reactjs.org/docs/getting-started.html)
+- [Styled-Components](https://styled-components.com)
+- [React Slick](https://react-slick.neostack.com)
+- [React Awesome Reveal](https://react-awesome-reveal.morello.dev/)
--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React, { Component } from 'react';
-import Header from './components/Header';
-import About from './components/About';
-import Resume from './components/Resume';
-import Portfolio from './components/Portfolio';
-import Testimonials from  './components/Testimonials';
-import ContactUs from './components/ContactUs';
-import Footer from './components/Footer';
-import resumeData from './resumeData';
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header resumeData={resumeData}/>
-        <About resumeData={resumeData}/>
-        <Resume resumeData={resumeData}/>
-        <Portfolio resumeData={resumeData}/>
-        <Testimonials resumeData={resumeData}/>
-        <ContactUs resumeData={resumeData}/>
-        <Footer resumeData={resumeData}/>
-      </div>
-    );
-  }
+import styled from "styled-components";
+import Header from "./components/Banner/Header";
+import ProfComponent from "./components/Banner/ProfComponent";
+import Clients from "./components/Clients/Clients";
+import Footer from "./components/Footer/Footer";
+import Projects from "./components/Projects/Projects";
+import Services from "./components/Service/Services";
+function App() {
+  return (
+    <Container>
+      <Banner>
+        <Header />
+        <ProfComponent />
+      </Banner>
+      <Services />
+      <LightColor>
+        <Projects />
+      </LightColor>
+      <Clients />
+      <LightColor>
+        <Footer />
+      </LightColor>
+    </Container>
+  );
 }
 
 export default App;
+
+const Container = styled.div``;
+const Banner = styled.div`
+  background: linear-gradient(159deg, rgb(45, 45, 58) 0%, rgb(43, 43, 53) 100%);
+  height: 100vh;
+  @media (max-width: 640px) {
+    height: 100%;
+    padding-bottom: 2rem;
+  }
+`;
+
+const LightColor = styled.div`
+  background: linear-gradient(159deg, rgb(45, 45, 58) 0%, rgb(43, 43, 53) 100%);
+`;
