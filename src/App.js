424b7d486fa5d2b0a8644e5d0d2b77a866844c54
vaibhavpatidar522 <vaibhavpatidar522@gmail.com>
2023-04-17 17:36:19 +0530

    firebas functon remove

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Banner/Header";
 import ProfComponent from "./components/Banner/ProfComponent";
 import Clients from "./components/Clients/Clients";
 import Footer from "./components/Footer/Footer";
-import Projects from "./components/Projects/Projects";
+// import Projects from "./components/Projects/Projects";
 import Services from "./components/Service/Services";
 function App() {  
   return (
@@ -13,13 +13,17 @@ function App() {
         <ProfComponent />
       </Banner>
       <Services />
-      <LightColor>
+      {/* <LightColor>
         <Projects />
       </LightColor>
       <Clients />
       <LightColor>
         <Footer />
+      </LightColor> */}
+      <LightColor>
+      <Clients />
       </LightColor>
+        <Footer />
     
     </Container>
     
--- src/components/Clients/Clients.jsx
+++ src/components/Clients/Clients.jsx
@@ -4,10 +4,10 @@ import styled from "styled-components";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { Slide } from "react-awesome-reveal";
 import Card from "../Card/Card";
-import { db } from "../../config/firebase";
-import { useState } from "react";
-import { useEffect } from "react";
-import { collection,  getDocs } from "firebase/firestore";
+// import { db } from "../../config/firebase";
+// import { useState } from "react";
+// import { useEffect } from "react";
+// import { collection,  getDocs } from "firebase/firestore";
 
 
 var settings = {
@@ -47,24 +47,53 @@ var settings = {
 };
 
 const Clients = () => {
-  const [newsList, setNewsList] = useState([]);
+  const newsList = [{
+    title: "Blog Post 1",
+    image: "https://images.unsplash.com/photo-1485827404703-89b55fcc595e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer volutpat mi sed enim vulputate, nec interdum sem facilisis. Morbi congue fermentum odio ac euismod. Praesent efficitur magna id elit molestie, in euismod nibh facilisis. Nullam at libero justo. ",
+    url: "https://example.com/post1",
+  },
+  {
+    title: "Blog Post 1",
+    image: "https://images.unsplash.com/photo-1535303311164-664fc9ec6532?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80   ",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer volutpat mi sed enim vulputate, nec interdum sem facilisis. Morbi congue fermentum odio ac euismod. Praesent efficitur magna id elit molestie, in euismod nibh facilisis. Nullam at libero justo. ",
+    url: "https://example.com/post1",
+  },
+  {
+    title: "Blog Post 1",
+    image: "https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1415&q=80",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer volutpat mi sed enim vulputate, nec interdum sem facilisis. Morbi congue fermentum odio ac euismod. Praesent efficitur magna id elit molestie, in euismod nibh facilisis. Nullam at libero justo. ",
+    url: "https://example.com/post1",
+  },
+  {
+    title: "Blog Post 1",
+    image: "https://images.unsplash.com/photo-1585829365295-ab7cd400c167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer volutpat mi sed enim vulputate, nec interdum sem facilisis. Morbi congue fermentum odio ac euismod. Praesent efficitur magna id elit molestie, in euismod nibh facilisis. Nullam at libero justo. ",
+    url: "https://example.com/post1",
+  },]
 
-  const newsCollection = collection(db, "News");
-  useEffect(() => {
-    const getNewsList = async () => {
-      try {
-        const data = await getDocs(newsCollection);
-        const filteredData = data.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-        setNewsList(filteredData);
-      } catch (e) {
-        console.error(e);
-      }
-    };
-    getNewsList();
-  }, [newsCollection]);
+  // const [newsList, setNewsList] = useState([]);
+
+  // const newsCollection = collection(db, "News");
+  // useEffect(() => {
+  //   const getNewsList = async () => {
+  //     try {
+  //       const data = await getDocs(newsCollection);
+  //       const filteredData = data.docs.map((doc) => ({
+  //         ...doc.data(),
+  //         id: doc.id,
+  //       }));
+  //       setNewsList(filteredData);
+  //     } catch (e) {
+  //       console.error(e);
+  //     }
+  //   };
+  //   getNewsList();
+  // }, [newsCollection]);
 
   const arrowRef = useRef(null);
   //     let clientDisc = "";
